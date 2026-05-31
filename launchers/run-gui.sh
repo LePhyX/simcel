235922742ea32affd872357e8ce8b3c5a7d0c02d
rmouahid <prorayanmouahid@gmail.com>
2026-05-31 21:24:45 +0200

    correction on run-gui.sh

--- launchers/run-gui.sh
+++ launchers/run-gui.sh
@@ -7,5 +7,5 @@ set -e # Exit on error
 
 cd "$(dirname "$0")/.." # Se placer à la racine du projet pour que Maven puisse trouver le pom.xml
 
-mvn compile -q # Compiler le projet sans afficher les logs détaillés
-mvn exec:java -q  # Lancer l'application en mode graphique (pas de --headless)
+mvn compile -q    # Compiler le projet sans afficher les logs détaillés
+mvn javafx:run -q # Lancer l'application en mode graphique via le plugin JavaFX
